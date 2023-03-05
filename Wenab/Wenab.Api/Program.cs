7dcb78eb0d01164a24e2fe388b09a39de871a7d5
Steve Yeager <steve.f.yeager@gmail.com>
2023-03-05 09:11:13 -0800

    Get the shared budget

--- Wenab/Wenab.Api/Program.cs
+++ Wenab/Wenab.Api/Program.cs
@@ -1,19 +1,19 @@
 using LittleByte.Common;
 using LittleByte.Common.AspNet.Middleware;
-using LittleByte.Common.Logging.Configuration;
 using LittleByte.Common.AspNet.Configuration;
-using LittleByte.Common.Identity.Configuration;
+using LittleByte.Common.Configuration;
+using Wenab.Ynab;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services
-    .AddLogs()
     .AddOpenApi("Wenab")
     .AddSingleton<IDateService, DateService>()
     .AddEndpointsApiExplorer()
-    .AddSwaggerGen()
-    .AddJwtAuthentication(builder.Configuration);
+    .AddSwaggerGen();
+
+builder.Services.BindOptions<YnabOptions>(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,8 +21,6 @@ app
     .UseHttpsRedirection()
     .UseHsts()
     .UseRouting()
-    .UseAuthentication()
-    .UseAuthorization()
     .UseHttpExceptions()
     .UseModelValidationExceptions()
     .UseEndpoints(endpoints => endpoints.MapControllers())
