94895c93b65f1713df115da8cc1640ffaf834f70
Steve Yeager <steve.f.yeager@gmail.com>
2023-03-05 09:36:21 -0800

    Add TS client generation (#1)
    * Add TS client generation
    
    * Delete .eslintignore
    
    * Review feedback
    
    ---------
    
    Co-authored-by: rsimm0711 <57923836+rsimm0711@users.noreply.github.com>

---
+++ wenab-app/tools/generateClient.ts
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const commander = require("commander");
+const dotenvFlow = require("dotenv-flow");
+const path = require("path");
+const child_process = require("child_process");
+const prependFile = require("prepend-file");
+const fs = require("fs");
+const clientName = "WenabClient";
+
+loadEnv();
+const options = loadOptions();
+runNswag(options);
+postProcessClient();
+console.info("Client generation complete");
+
+function loadEnv() {
+  const mode = process.env.NODE_ENV || "development";
+  console.info(`Mode=${mode}`);
+
+  const env = dotenvFlow.config({ node_env: mode, default_node_env: mode });
+  if (env.error) {
+    throw new Error(env.error.message);
+  } else {
+    console.info(env.parsed);
+  }
+}
+
+function loadOptions() {
+  commander.option("-u, --url <url>,", "API URL", process.env.API_URL);
+  commander.option(
+    "-s, --scheme <scheme>,",
+    "HTTP/HTTPS",
+    process.env.API_SCHEME
+  );
+  commander.option("-d --dotnet <dotnet>", "Dotnet Runtime", process.env.DOTNET_RUNTIME);
+  commander.parse(process.argv);
+
+  const options = commander.opts();
+  console.info(JSON.stringify(options));
+
+  return options;
+}
+
+function runNswag(options: any) {
+  function onComplete(
+    error: never | null,
+    stdout: string,
+    stderr: string
+  ): void {
+    if (stdout !== "") {
+      console.log(stdout);
+    }
+
+    if (stderr !== "") {
+      console.error(stderr);
+    }
+
+    if (error != null) {
+      throw error;
+    }
+  }
+
+  console.info("Running nswag");
+  const nswagPath = path.join("node_modules", ".bin", "nswag");
+  const variables = `/variables:API_URL=${options.url},SCHEME=${options.scheme},DOTNET_RUNTIME=${options.dotnet}`;
+  const command = `${nswagPath} run ./config/${clientName}.nswag ${variables}`;
+  child_process.execSync(command, onComplete);
+}
+
+function postProcessClient() {
+  const configPath = path.join(
+    __dirname,
+    "..",
+    "config",
+    `${clientName}.nswag`
+  );
+  const nswagConfigString = fs.readFileSync(configPath, "utf8");
+  const nswagConfig = JSON.parse(nswagConfigString);
+  const outputFile =
+    nswagConfig.codeGenerators.openApiToTypeScriptClient.output;
+  const outputPath = path.join(__dirname, "..", "config", outputFile);
+  console.info(`Generated client output file at ${outputPath}`);
+  console.info(
+    "Editing generated client file to be ignored by the ts compiler"
+  );
+  prependFile(outputPath, "/* eslint-disable */\n// @ts-nocheck\n");
+}
