315e547b8f559ddf3660378e4cbd0aef77979f5d
Steve Yeager <steve.f.yeager@gmail.com>
2023-03-05 22:32:13 -0800

    Add basic budget and category DTOs

---
+++ Wenab/Wenab.Api/Budgets/BudgetDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Wenab.Api.Budgets;
+
+public sealed record CategoryDto(string Name, long Balance);
+
+public sealed record BudgetDto(IReadOnlyList<CategoryDto> Categories);
--- Wenab/Wenab.Api/Budgets/GetBudgetController.cs
+++ Wenab/Wenab.Api/Budgets/GetBudgetController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LittleByte.Common.AspNet.Responses;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Wenab.Ynab;
 using Controller = LittleByte.Common.AspNet.Core.Controller;
@@ -15,7 +16,7 @@ public sealed class GetBudgetController : Controller
     }
 
     [HttpGet]
-    public async Task<IActionResult> Get()
+    public async Task<ApiResponse<BudgetDto>> Get()
     {
         var client = new HttpClient
         {
@@ -24,7 +25,9 @@ public sealed class GetBudgetController : Controller
 
         var ynabClient = new Client(client);
         var budget = await ynabClient.GetBudgetByIdAsync(options.SharedBudgetId, null);
+        var categories = budget.Data.Budget.Categories.Select(c => new CategoryDto(c.Name, c.Balance)).ToArray();
+        var budgetDto = new BudgetDto(categories);
 
-        return Ok(budget);
+        return new OkResponse<BudgetDto>(budgetDto);
     }
 }
